f27914291e5c36f05e6fb287a90f544dd8342d15
Vlad Afanasev <vlad-vladik-afanasev@mail.ru>
2025-01-23 19:32:43 +0300

    Added BatchController class

---
+++ src/main/java/ru/alishev/springcourse/controllers/BatchController.java
@@ -0,0 +1,39 @@
+package ru.alishev.springcourse.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.alishev.springcourse.dao.PersonDAO;
+
+@Controller
+@RequestMapping("/test-batch-update")
+public class BatchController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public BatchController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping
+    public String index() {
+        return "batch/index";
+    }
+    @GetMapping("/without")
+    public String withoutBatch() {
+        personDAO.testMultipleUpdate();
+        return "redirect:/people";
+    }
+    @GetMapping("/with")
+    public String withBatch() {
+        personDAO.testBatchUpdate();
+        return "redirect:/people";
+    }
+    @GetMapping("/clear")
+    public String clearTable() {
+        personDAO.clearTable();
+        return "redirect:/people"; // Перенаправляем на список людей после очистки
+    }
+}
--- src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -1,6 +1,7 @@
 package ru.alishev.springcourse.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -31,7 +32,7 @@ public class PersonDAO {
     }
 
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person VALUES(1,?,?,?)",
+        jdbcTemplate.update("INSERT INTO Person(name,age,email) VALUES(?,?,?)",
                 person.getName(), person.getAge(), person.getEmail());
     }
 
@@ -42,5 +43,56 @@ public class PersonDAO {
 
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+    }
+
+    // ТЕСТ ПАКЕТНОЙ ВСТАВКИ
+
+    public void testMultipleUpdate() {
+        List<Person> people = create1000people();
+        long before = System.currentTimeMillis();
+        for(Person person : people) {
+            jdbcTemplate.update("INSERT INTO Person VALUES(?,?,?,?)",
+                    person.getId(),person.getName(),person.getAge(),person.getEmail());
+        }
+        long after = System.currentTimeMillis();
+        System.out.println("Time taken: " + (after - before));
+
+    }
+
+    private List<Person> create1000people() {
+        List<Person> people = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            people.add(new Person(i,"Name" + i, 30, "test" + i + "@test.com"));
+        }
+        return people;
+    }
+
+    public void testBatchUpdate() {
+        List<Person> people = create1000people();
+        long before = System.currentTimeMillis();
+
+        jdbcTemplate.batchUpdate("INSERT INTO Person VALUES (?,?,?,?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+                        preparedStatement.setInt(1, people.get(i).getId());
+                        preparedStatement.setString(2, people.get(i).getName());
+                        preparedStatement.setInt(3, people.get(i).getAge());
+                        preparedStatement.setString(4, people.get(i).getEmail());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return 1000;
+                    }
+                });
+
+        long after = System.currentTimeMillis();
+
+        System.out.println("Time taken: " + (after - before));
+    }
+
+    public void clearTable() {
+        jdbcTemplate.update("DELETE FROM Person");
     }
 }
